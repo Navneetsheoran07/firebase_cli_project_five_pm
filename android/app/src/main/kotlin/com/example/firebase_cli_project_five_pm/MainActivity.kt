382e1bf7e2797338283c2774a9e85e9828ba4aee
Navneet sheoran <navneetsheoran537@gmail.com>
2023-08-29 17:41:59 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/firebase_cli_project_five_pm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebase_cli_project_five_pm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/controller/auth_controller.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+
+import 'package:get/get.dart';
+
+import '../pages/signuppage.dart';
+import 'auth_with_number.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    authWithNumber controller = Get.put(authWithNumber());
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Home Page"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Get.to(SingupPage());
+              },
+              icon: Icon(Icons.person),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Image.asset("assets/images/home.png"),
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    child: Icon(
+                      Icons.person,
+                      size: 70,
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text("Name"),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Text("Ansd@gmail.com"),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Obx(
+                                () => Text(
+                              controller.userMobilerNumber.toString(),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {
+                  controller.logoutUser();
+                },
+                child: Text("LOGOUT"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Move To Next"),
+              )
+            ],
+          ),
+        ));
+  }
+}
